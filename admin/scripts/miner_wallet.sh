bad1238c46714eb22948abbcaacb1f93e61b56ed
Matthew Zipkin <pinheadmz@gmail.com>
2025-01-29 15:31:46 -0500

    update for BOSS 2025 teams

--- admin/scripts/miner_wallet.sh
+++ admin/scripts/miner_wallet.sh
@@ -1,2 +1,2 @@
 warnet bitcoin rpc miner createwallet miner
-warnet bitcoin rpc miner importdescriptors '[{\"desc\":\"combo\(cNccWAfPNyW6gDtrgDqyhdEL52Ps7anBMy6Yv1HL4EUMbUKQPKaX\)#rvzcntm0\"\,\"timestamp\":0}]'
+warnet bitcoin rpc miner importdescriptors '[{\"desc\":\"combo\(cULtWtusk3PZirb6J4BQfu1wFn1SfZznFbpJR1t4mNpRXy75a16v\)#xlctt9nm\"\,\"timestamp\":0}]'
